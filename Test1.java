73455d3fdea9f0c9891338fbcbb2bb5342e0f632
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-06-18 17:05:02 -0400

    when i>j

--- Test1.java
+++ Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 public static void main(String[] args) 
 {
 System.out.println("SUBSTRING FOR KIDS");
-String sentence= substringForKids(1,2,"fun");
+String sentence= substringForKids(2,1,"fun");
 System.out.println(sentence);
 }
 
@@ -17,9 +17,10 @@ public static String substringForKids(int i, int j, String k)
 		for(int kiddo=i; kiddo<=j;kiddo++)
 			{
 				kids+=k.charAt(kiddo);
-			}/*
+			}
 	if(i>j)
 		return "you have gave me invalid numbers!";
+			/*
 	if(i<0)
 		return "you gave me invalid numbers!";*/
 
